5381a7582b5d73db534a0c1f8dfa30f784d4f67b
YI QI <30916366+TinyBookE@users.noreply.github.com>
2019-10-13 18:57:19 +0800

    Delete Table.py
    Not used

--- week2/Table.py
+++
@@ -1,7 +0,0 @@
-import sqlalchemy
-from sqlalchemy.ext.declarative import declarative_base
-Base = declarative_base()
-
-class Table(Base):
-    __tablename__ = 'data'
-    date = sqlalchemy.column()
